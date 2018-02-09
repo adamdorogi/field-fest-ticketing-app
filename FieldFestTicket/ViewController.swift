a0bb0187c21d13e0950884dc00f54c37aa54bd8f
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2018-02-09 13:02:01 +0100

    Initial Commit

---
+++ FieldFestTicket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FieldFestTicket
+//
+//  Created by Adam Dorogi-Kaposi on 9/2/18.
+//  Copyright © 2018 Adam Dorogi-Kaposi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
